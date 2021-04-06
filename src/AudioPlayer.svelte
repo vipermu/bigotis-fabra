a57a28ad544f34e8ae6b1a1d2de203c27d08276c
Victor <vipermu97@gmail.com>
2021-04-06 18:01:38 +0200

    first commit

---
+++ src/AudioPlayer.svelte
@@ -0,0 +1,40 @@
+<script context="module">
+    import { onMount } from 'svelte'
+
+    const players = new Set()
+    
+  
+    export function stopAll() {
+      players.forEach(p => p.pause())
+    }
+</script>
+
+<script>
+    
+    let player
+    export let src
+
+    onMount(() => {
+      // Like players.push(player)
+    //   players.add(player)
+      player.play()
+    })
+  
+</script>
+
+<style>
+    audio {
+        opacity: 0;
+        position: absolute;
+    }
+
+</style>
+  
+<div>
+    <audio
+        bind:this={player}
+        {src}
+        controls>
+        <track kind="captions" />
+    </audio>
+</div>
